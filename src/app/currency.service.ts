a47920ee489142d47ff9e5c01ee9d6c3b37972c3
Mortis <lucas.wiese@gmx.de>
2020-06-21 10:28:08 +0200

    new layout

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { combineLatest, Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class CurrencyService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    endpoint = "convert";
-    access_key = "9420160af2030efa44b6226c602afa98";
-    private baseUrl: string = "http://data.fixer.io/api/";
+  private accessKey = '9420160af2030efa44b6226c602afa98';
+  private baseUrl = 'http://data.fixer.io/api/';
+  allSymbols$ = new BehaviorSubject(null);
 
+  // & base = GBP
+  // & symbols = USD,CAD,EUR
+  getAllCurrencySymbols() {
+    return this.http
+      .get(this.baseUrl + 'symbols?access_key=' + this.accessKey)
+      .pipe(
+        map((x: { success: boolean; symbols?: {}; error?: {} }) => {
+          if (!x.success) {
+            throw x.error as Error;
+          }
+          const symbols = Object.values(x.symbols);
+          const codes = Object.keys(x.symbols);
 
-    getCurrencySymbols() {
-        return this.http.get(
-            this.baseUrl + "symbols?access_key=" + this.access_key
-        );
-    }
+          this.allSymbols$.next(
+            codes.map((c, i) => {
+              return { code: c, symbol: symbols[i] };
+            })
+          );
+        })
+      );
+  }
+  private getCurrency() {
+    return this.http
+      .get(this.baseUrl + 'latest?access_key=' + this.accessKey)
+      .pipe(
+        map((x: { success: boolean; rates?: {}; error?: {} }) => {
+          if (!x.success) {
+            throw x.error as Error;
+          }
+          return x.rates;
+        })
+      );
+  }
 
-    getCurrency() {
-        return this.http.get(this.baseUrl + "latest?access_key=" + this.access_key);
-    }
+  // getAll() {
+  //   return combineLatest([
+  //     this.getCurrency(),
+  //     this.getAllCurrencySymbols(),
+  //   ]).pipe(
+  //     map((x) => {
+  //       const codes = Object.keys(x[0]);
+  //       const rates = Object.values(x[0]);
+  //       const symbols = Object.values(x[1]);
+
+  //       return codes.map((c, i) => {
+  //         return { code: c, rate: rates[i], symbol: symbols[i] };
+  //       });
+  //     })
+  //   );
+  // }
 }
